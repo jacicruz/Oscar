939840558fb909f54e3fc2f6989484ccb195bf3d
jacicruz <jaoc020211@gmail.com>
2023-09-09 18:56:30 -0600

    se actualizaron algunos cambios

--- myFirstLibrary/src/Calculadora/division.java
+++ myFirstLibrary/src/Calculadora/division.java
@@ -3,20 +3,23 @@ package Calculadora;
 import javax.swing.JOptionPane;
 
 public class division {
-    public void dividir(float num, float num2){
-    if(num2 != 0.0){
-        JOptionPane.showMessageDialog(null, "El resultado de la division es: "+(num/num2));
-    } else{
-        JOptionPane.showMessageDialog(null, "No existe la division entre 0");
+
+    public void dividir(float num, float num2) {
+        if (num2 != 0.0) {
+            JOptionPane.showMessageDialog(null, "El resultado de la division es: " + (num / num2));
+        } else {
+            JOptionPane.showMessageDialog(null, "No existe la division entre 0");
+        }
     }
-    }
-    public float division_retorno (float num, float num2){
-    if(num2 != 0.0){
-    return(num/num2);
-    } else{
-        JOptionPane.showMessageDialog(null, "No existe la division entre 0");
-        System.exit(0);
-    }
+
+    public float division_retorno(float num, float num2) {
+        if (num2 != 0.0) {
+            return (num / num2);
+        } else {
+            JOptionPane.showMessageDialog(null, "No existe la division entre 0");
+            System.exit(0);
+        }
         return 0;
+
     }
 }
